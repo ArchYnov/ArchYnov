7968be0619d28319f6a73f343579c959ae0aabd1
loux22 <louis.ardilly@yahoo.fr>
2023-01-19 23:43:14 +0100

    add library mongo (To be completed)

---
+++ tools/mongo.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+# A Completer
+class Mongo():
+
+    def __init__(self):
+        self.client = MongoClient("mongodb://root:root@mongo:27017/")
+        self.db = self.client["ydays"]
+
+    def getCollection(self, name_collection):
+        """get collection
+
+        Args:
+            name_collection (str): name of the collection
+
+        Returns:
+            collection: collection
+        """
+
+        collection = self.db[name_collection]
+        collection = self.db.get_collection(name_collection)
+        return collection
+    
+
+    def insertOne(self, name_collection, data):
+        """insert un element en base de données
+
+        Args:
+            name_collection (str): Name of the collection
+            data (dict): data insert in bdd
+        """
+        print("insert")
+        collection = self.getCollection(name_collection)
+        collection.insert_one(data)  
