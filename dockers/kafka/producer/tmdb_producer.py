255167340ef6019db5cf220f007999d5b9c9f2a4
loux22 <louis.ardilly@yahoo.fr>
2023-02-15 23:01:55 +0100

    [Add] creation du producer-tmdb

---
+++ dockers/kafka/producer/tmdb_producer.py
@@ -0,0 +1,18 @@
+from json import dumps, loads
+from kafka import KafkaProducer
+from time import sleep
+import requests
+
+producer = KafkaProducer(bootstrap_servers="kafka:29092",value_serializer=lambda x: dumps(x).encode("utf-8"))
+
+while True:
+    try:
+        # on recupere le content de la route fetchTmdb que l'on convertit de bytes -> list(dict)
+        contents = requests.get('http://python2:5002/fetchTmdb').content
+        contents = loads(contents.decode("utf-8"))
+        for content in contents["result"]:
+            producer.send("tmdb", content)
+    except:
+        print("erreur lors de la récupération de l'api")
+    # A MODIFIER, on veut pas recuperer les films toutes les 5 secondes
+    sleep(5)
