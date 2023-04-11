4c6f93dbd754c1bc242bdeae716c126029e3d182
loux22 <louis.ardilly@yahoo.fr>
2023-04-11 21:51:23 +0200

    merge

--- dockers/python-twitter/main.py
+++ dockers/python-twitter/main.py
@@ -4,14 +4,11 @@ import json
 from tools.redis_client import RedisClient
 from tools.mongo import MongodbClient
 from feeds.twitterClient import TwitterClient
-<<<<<<< HEAD
 from fastapi import FastAPI
 from fastapi import Response
 import json
 import uvicorn
 from datetime import datetime
-=======
->>>>>>> 1835a7e586079b51603a848a2aff649a045892b1
 
 app = FastAPI()
 
@@ -33,27 +30,15 @@ async def fetchTwitter():
         OUT  : result of the request
     """
     # Fetch mongoDB movie list
-<<<<<<< HEAD
 
     testCol = client_mongo.getAllDocumentsFromCollection("tmdb", column={"_id": 1, "original_title": 1})
     all_movies = [{"_id": ele["_id"], "original_title": ele["original_title"]} for ele in testCol]
     tweets = []
-=======
-    testCol = client_mongo.getAllDocumentsFromCollection("tmdb")
-    all_movies = [ el["original_title"] for el in testCol ]
->>>>>>> 1835a7e586079b51603a848a2aff649a045892b1
     for movie_name in all_movies:
         query = '#' + movie_name["original_title"].replace(' ', '') + ' -filter:retweets'
         tweets += twitter_feed.pushNewTweets(query=query , count=2, movie_id=movie_name["_id"])
     return Response(
-<<<<<<< HEAD
             status_code=200,
             content=json.dumps({"result": tweets}, default=datetime_handler),
             media_type="application/json"
         )
-=======
-        status_code=200,
-        content=json.dumps({"result": "sucess"}),
-        media_type="application/json"
-    )
->>>>>>> 1835a7e586079b51603a848a2aff649a045892b1
