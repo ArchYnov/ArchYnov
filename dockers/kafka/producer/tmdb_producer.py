bfe7fea595f7043fe669df79950a11a5563fd269
Paul <paul.bagnis@gmail.com>
2023-04-12 19:50:23 +0200

    [edit] refacto des enfers (regarde comment on fait Fanfoué)

--- dockers/kafka/producer/tmdb_producer.py
+++ dockers/kafka/producer/tmdb_producer.py
@@ -3,18 +3,26 @@ from kafka import KafkaProducer
 from time import sleep
 import requests
 from requests import ConnectionError
+from tools.redis_client import RedisClient
+from tools.mongo import MongodbClient
+from feeds.tmdbClient import TMDbClient
 
-print(" - Application started!")
+print(" TMDB - Application started!")
 producer = KafkaProducer(bootstrap_servers="kafka:29092",value_serializer=lambda x: dumps(x).encode("utf-8"))
+client_redis = RedisClient(host="redis")
+mongo_client= MongodbClient()
+tmdb_feed = TMDbClient(
+    mongo_client=mongo_client,
+    api_key=client_redis.get_value_by_key(["api_key_tmdb"])['api_key_tmdb']
+)
 print("Producer initialized.")
 
-url = 'http://python-tmdb:5001/fetchTmdb'
 while True:
     try:
-        print(f"Fetching -> {url}")
-        contents = loads(requests.get(url).content)
+        print(f"Fetching ...")
+        contents = tmdb_feed.fetchNewMovies()
         print(f"Fetch sucessfull ! Sending data to consumers...")
-        for content in contents["result"]:
+        for content in contents:
             producer.send("tmdb", content)
         print(f"All the data has beem sent.")
         wait_time = 60*60
