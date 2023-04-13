e22a96db0e3dab141f1d08c2bc41b8161153c9f3
loux22 <louis.ardilly@yahoo.fr>
2023-04-13 11:00:09 +0200

    [edit] change type date in mongo tmdb

--- dockers/kafka/consumer/tmdb_consumer.py
+++ dockers/kafka/consumer/tmdb_consumer.py
@@ -1,6 +1,7 @@
 from json import loads
 from kafka import KafkaConsumer
 from tools.mongo import MongodbClient
+from datetime import datetime
 
 print(" - Application started!")
 
@@ -18,6 +19,7 @@ for value in consumer:
     # on convertit les datas envoyer par le consumer bytes -> dict
     data = loads(value.value.decode("utf-8"))
     try:
+        data["release_date"] = datetime.strptime(data["release_date"], '%Y-%m-%d')
         MONGODB_CLIENT.insertOne("tmdb", data, CHECK_DUPLICATES)
         print("insertion en base réussis")
     except:
