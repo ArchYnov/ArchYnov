243f3270d11fb0ef64332e8839e1a0399189d2ec
loux22 <louis.ardilly@yahoo.fr>
2023-04-19 10:39:41 +0200

    add model sentiment

--- dockers/pipeline-ml/main.py
+++ dockers/pipeline-ml/main.py
@@ -19,35 +19,46 @@ In term of database we use MongoDB (https://www.mongodb.com/), as we said before
 """
 
 import time
+import pandas as pd
 from tools.mongo import MongodbClient
 from transformers import pipeline
+from sentiment_analysis import SentimentAnalysis
 
 client_mongo = MongodbClient()
-sentiment_pipeline = pipeline("sentiment-analysis")
+# sentiment_pipeline = pipeline("sentiment-analysis")
 
 def main():
     count_loop = 0
     while True: 
         # set query to fetch the documents where Sentiment Analysis isn't defined
+
         query = { '_sentiment_analysis': 'n/a' }
+        # query = {}
         # send query
         print("Fetching datas...")
         tweets       = client_mongo.get_documents('tweets', query)
         rss_articles = client_mongo.get_documents('rss', query)
-
         print("Fetch done !")
+        if len(tweets) > 0:
+            tweet_analysed = SentimentAnalysis(pd.DataFrame(tweets, columns=["_id", "text"]))
+            tweet_analysed = tweet_analysed.calcul_sentiment()
+            print("analyse tweet")
+            for index, value in enumerate(tweets):
+                client_mongo.update_one('tweets', { "_id" : value[0] }, { "$set" : { "_sentiment_analysis" : tweet_analysed[index] } })
 
-        tweet_analysed = sentiment_pipeline([ele[1] for ele in tweets])
-        rss_analysed = sentiment_pipeline([ele[1] for ele in rss_articles])
+        if len(rss_articles) > 0:
+            rss_analysed = SentimentAnalysis(pd.DataFrame(rss_articles, columns=["_id", "text"]))
+            rss_analysed = rss_analysed.calcul_sentiment()
+            print("analyse rss")
+            for index, value in enumerate(rss_articles):
+                client_mongo.update_one('rss', { "_id" : value[0] }, { "$set" : { "_sentiment_analysis" : rss_analysed[index] } })
 
-        for index, value in enumerate(tweets):
-            client_mongo.update_one('tweets', { "_id" : value[0] }, { "$set" : { "_sentiment_analysis" : tweet_analysed[index] } })
-        for index, value in enumerate(rss_articles):
-            client_mongo.update_one('rss', { "_id" : value[0] }, { "$set" : { "_sentiment_analysis" : rss_analysed[index] } })
+        # tweet_analysed = sentiment_pipeline([ele[1] for ele in tweets])
+        # rss_analysed = sentiment_pipeline([ele[1] for ele in rss_articles])
 
         count_loop += 1
-        print(f'Added sentiment analysis for {len(tweet_analysed)} tweets and {len(rss_analysed)} rss articles.')
-        if len(tweet_analysed) > 0 or len(rss_analysed) > 0 :
+        print(f'Added sentiment analysis for {len(tweets)} tweets and {len(rss_articles)} rss articles.')
+        if len(rss_articles) > 0 or len(tweets) > 0 :
             time.sleep(60)
         else :
             time.sleep(60*60-(60 * count_loop))
